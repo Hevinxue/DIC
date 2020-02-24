74cb99022d17ed37496a40a359372b0f9c15ceb7
Hevinxue <Hevinxue2@163.com>
2020-02-24 14:29:01 -0500

    Update and rename temp.py to FreqDomainFilterAndShift.py

--- FreqDomainFilterAndShift.py
+++ FreqDomainFilterAndShift.py
@@ -1,34 +1,16 @@
-# from multiprocessing import Process
-# import os
-#
-# # 子进程要执行的代码
-# def run_proc(name):
-#     print('Run child process %s (%s)...------------------------------------------------------------> SON' % (name, os.getpid()))
-#
-# if __name__=='__main__':
-#     print('Parent process %s.' % os.getpid())
-#     p = Process(target=run_proc, args=('test',))
-#     print('Child process will start.---0')
-#     p.start()
-#     print('---------------------------->A')
-#     p.join()  # join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步
-#     print('Child process end.---------------------------->B')
-
-
-import DIC_Speckle_Quality_Assessment as SQA
+# Basic staff
 from skimage import io
-import random
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 import numpy as np
 
-
 plt.figure()
-a = io.imread(r'C:\Users\Lenovo\Desktop\PyLearning\My_Files\Monterey.jpg', as_gray=True)  # read in pics
-# monterey = SQA.Speckle(a)
+a = io.imread(r'C:\Users\blabla', as_gray=True)  # read in pics
 plt.subplot(3,2,1)
 plt.imshow(a , cmap='gray',)
 
+
+# Freq domain filter
 a_fft = np.fft.fft2(a)
 a_fft_raw = a_fft
 a_fft = np.fft.fftshift(a_fft)
@@ -53,7 +35,7 @@ plt.imshow(a_filted, cmap='gray',)
 
 
 
-# subpixel shift
+# (Subpixel)Shift operated in freq domain
 plt.figure()
 
 row_shift = 400.0
